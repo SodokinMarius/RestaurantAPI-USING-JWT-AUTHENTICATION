cf1435459d34d9eb007ebaa3884f9ba6dd667bdb
SodokinMarius <yaomariussodokin@gmail.com>
2022-09-02 01:12:47 +0100

    refactor: updating restaurant urls

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,18 +2,16 @@
 from django.contrib import admin
 from django.urls import path,include
 
-from . import views
-from rest_framework.routers import DefaultRouter 
+from .viewsets import RestaurantViewSet
+from rest_framework.routers import SimpleRouter 
 
+
+routeur=SimpleRouter()
+
+routeur.register('restaurants/',RestaurantViewSet,basename='restaurants-list')
 
 urlpatterns=[ 
-            path('restaurants/',views.RestaurantAPIView.as_view(),name='restaurants-list'),
-            path('restaurant/<int:pk>/',views.RestaurantDetail.as_view(),name='single-restaurant'),
-             
-             #Urls pour obtenir la liste des utilisateurs
-            #path('users/', views.UserList.as_view()),
-            #path('users/<int:pk>/', views.UserDetail.as_view()),
+            path('',include(routeur.urls)),
              
              ]
 
-#urlpatterns = format_suffix_patterns(urlpatterns)
