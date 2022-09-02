b3bef7823f7a2ee7e6e21d13b09f6e62f6a7b298
SodokinMarius <yaomariussodokin@gmail.com>
2022-09-02 10:35:40 +0100

    refactor : update RestaurantSerializer

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -5,11 +5,10 @@ from .models import Restaurant
 from authentification.models import User
 
 class RestaurantSerializer(serializers.ModelSerializer):
-    owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all()) #<----------champ ajouté
 
-    #owner = serializers.ReadOnlyField(source='owner.username') #l'objectif est d'associer le user à l'action
+    owner = serializers.ReadOnlyField(source='owner.username') #l'objectif est d'associer le user à l'action
     class Meta:
         model=Restaurant
-        fields=('pk','name','description','lng','lat','created_at','updated_date','owner')  #Ajoutons le owner aux champs de serialization
+        fields=('id','name','description','lng','lat','created_at','updated_date','owner')  #Ajoutons le owner aux champs de serialization
         read_only_fields=('pk','created_at','updated_date','owner')
-        #lookup_field = "pk"
+        lookup_field = "id"
